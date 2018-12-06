417d3f639c83e9dd3bf52056762fb539373d8afa
cosmins97 <sacaci_cosmin2000@yahoo.com>
2018-12-06 14:18:35 +0200

    User class and context

---
+++ DataLayer/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLayer
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string FirstName { get; private set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string LastName { get; private set; }
+
+        [Required]
+        [StringLength(10)]
+        public string RegistrationNumber { get; private set; }
+
+        public string UserName { get; private set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
+        public string Password { get; private set; }
+
+    }
+}
---
+++ DataLayer/UsersContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
