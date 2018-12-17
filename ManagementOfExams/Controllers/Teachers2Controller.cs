2973e951f99e1c2c958765c9d4bb401e02fa2368
GavriloviciEugen <gavrilovici.eugen@gmail.com>
2018-12-17 13:27:16 +0200

    Controllers update
    Commented unused controllers in order to compile

--- ManagementOfExams/Controllers/Teachers2Controller.cs
+++ ManagementOfExams/Controllers/Teachers2Controller.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,4 +30,4 @@ namespace ManagementOfExams.Controllers
      
 
     }
-}
+}*/
--- ManagementOfExams/Controllers/TeachersController1.cs
+++ ManagementOfExams/Controllers/TeachersController1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -95,4 +95,4 @@ namespace ManagementOfExams.Controllers
             return View(teacher);
         }
     }
-}
+}*/
