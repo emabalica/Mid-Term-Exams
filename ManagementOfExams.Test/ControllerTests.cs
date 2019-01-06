8b908bd2832c459f40dc9708b4b23ca6f3906e25
GavriloviciEugen <gavrilovici.eugen@gmail.com>
2019-01-06 16:53:44 +0200

    Tests
    Added the start/models of tests

---
+++ ManagementOfExams.Test/ControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using ManagementOfExams.Controllers;
+using ManagementOfExams.Data;
+using ManagementOfExams.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ManagementOfExams.Test
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void Home_Controller_Test()
+        {
+            //Arrange
+            HomeController controllerUnderTest = new HomeController();
+
+            //Act
+            var resultAbout = controllerUnderTest.About() as ViewResult;
+            var resultIndex = controllerUnderTest.Index() as ViewResult;
+            var resultContact = controllerUnderTest.Contact() as ViewResult;
+            var resultPrivacy = controllerUnderTest.Privacy() as ViewResult;
+
+            //Assert
+            Assert.AreEqual(null,resultAbout.ViewName);
+            Assert.AreEqual(null, resultIndex.ViewName);
+            Assert.AreEqual(null, resultContact.ViewName);
+            Assert.AreEqual(null, resultPrivacy.ViewName);
+        }
+    }
+}
---
+++ ManagementOfExams.Test/DataTests.cs
@@ -0,0 +1,66 @@
+using System;
+using ManagementOfExams.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManagementOfExams.Test
+{
+    [TestClass]
+    public class DataTests
+    {
+        [TestMethod]
+        public void TestTeacher_With_Consturctor()
+        {
+            //Arrange
+            string firstName = "TestF";
+            string lastName = "TestL";
+            string userName = "TestU";
+            string password = "TestP";
+            string emailAdress = "TestE";
+
+
+
+            //Act
+            Teacher teacher = new Teacher(firstName, lastName, userName, password, emailAdress);
+
+            //Assert
+            Assert.AreEqual(firstName,teacher.FirstName);
+            Assert.AreEqual(lastName, teacher.LastName);
+            Assert.AreEqual(userName, teacher.UserName);
+            Assert.AreEqual(password,teacher.Password);
+            Assert.AreEqual(emailAdress,teacher.EmailAddress);
+        }
+
+        [TestMethod]
+        public void TestTeacher_WithOut_Consturctor()
+        {
+            //Arrange
+            string firstName = "TestF";
+            string lastName = "TestL";
+            string userName = "TestU";
+            string password = "TestP";
+            string emailAdress = "TestE";
+
+
+
+            //Act
+            Teacher teacher = new Teacher();
+
+            teacher.FirstName = firstName;
+            teacher.LastName = lastName;
+            teacher.EmailAddress = emailAdress;
+            teacher.UserName = userName;
+            teacher.Password = password;
+            Subject s = new Subject();
+            teacher.Subject = s; 
+
+            //Assert
+            Assert.AreEqual(firstName, teacher.FirstName);
+            Assert.AreEqual(lastName, teacher.LastName);
+            Assert.AreEqual(userName, teacher.UserName);
+            Assert.AreEqual(password, teacher.Password);
+            Assert.AreEqual(emailAdress, teacher.EmailAddress);
+            Assert.AreEqual(s,teacher.Subject);
+        }
+
+    }
+}
