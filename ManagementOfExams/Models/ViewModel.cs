20c6b94952974b265eaf3a9857e8a88afb144a84
emabalica <ema.balica@yahoo.com>
2019-01-16 00:11:10 +0200

    Add files via upload

---
+++ ManagementOfExams/Models/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementOfExams.Models
+{
+    public class ViewModel
+    {
+        public StudentModel student = new StudentModel();
+        public SubjectModel subject { get; set; }
+        public GradeModel grades { get; set; }
+        public ExamModel exams { get; set; }
+        public ExamModel teachers { get; set; }
+
+    }
+}
